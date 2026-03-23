4194aa8b086a2a87d46445a0119163f0546a4c4e
Octave <Octave.Lejeune@etu.univ-grenoble-alpes.fr>
2026-03-23 20:59:18 +0100

    fix model url img

--- Model/modeleJeu.js
+++ Model/modeleJeu.js
@@ -3,14 +3,12 @@ export class Jeu {
     #appid;
     #name;
     #playtimeforever;
-    #img_icon_url;
 
 
     constructor(appid, name, playtimeForever, img_icon_url) {
         this.#appid = appid;
         this.#name = name;
         this.#playtimeforever = playtimeForever;
-        this.#img_icon_url = img_icon_url;
     }
 
     getAppid() {
@@ -37,11 +35,4 @@ export class Jeu {
         this.#playtimeforever = playtimeforever;
     }
 
-    getImgIconUrl() {
-        return this.#img_icon_url;
-    }
-
-    setImgIconUrl(img_icon_url){
-        this.#img_icon_url = img_icon_url;
-    }
 }
