5961be4ed7dedec9ceb66aac4e4b8ca6ad2b426d
Octave <Octave.Lejeune@etu.univ-grenoble-alpes.fr>
2026-03-23 19:16:24 +0100

    resultat view completed

--- Views/view.js
+++ Views/view.js
@@ -12,9 +12,26 @@ export const view = {
 
     etoileFavoris: document.querySelector("#etoile"),
 
-
-
-
-
+    blocResultats: document.querySelector("#bloc-resultats")
 
 };
+
+
+
+export function afficherJeuxProposes(jeux) {
+    view.blocResultats.innerHTML = "";
+
+    jeux.forEach((jeu) => {
+    const imageUrl = `https://cdn.cloudflare.steamstatic.com/steam/apps/${jeu.appid}/header.jpg`;
+    const lienSteam = `https://store.steampowered.com/app/${jeu.appid}`;
+
+    view.blocResultats.innerHTML += `
+            <article class="carte-jeu">
+                <img src="${imageUrl}" alt="Image de ${jeu.name}">
+                <h3>${jeu.name}</h3>
+                <p>${jeu.categorie}</p>
+                <a href="${lienSteam}" target="_blank">Voir sur Steam</a>
+            </article>
+    `;
+    });
+}
